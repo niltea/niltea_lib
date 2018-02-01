d4219a627120e3539dbf247eecc09574b9a4d97c
niltea <futurabodoni@gmail.com>
2018-02-01 20:44:26 +0900

    なんとなくブラウザ環境取得が正しく動く感じ

--- src/class/browserEnv.ts
+++ src/class/browserEnv.ts
@@ -1,26 +1,41 @@
-// UserAgentを取得
-const ua: string = navigator.userAgent.toLowerCase();
-
 // 文字列からバージョンを取得する
-const getVer= (argAgent: string, sarchStr: string, endstr: string): string => {
+const getVer = (argAgent: string, sarchStr: string, endstr: string = null): string => {
   const agent = argAgent.toLowerCase();
 
   // 開始位置を検出
   const start: number = agent.indexOf(sarchStr);
+  const sliceStart: number = start + sarchStr.length;
+
+  // もし見つからなかったらundefを返す
   if (start < 0) return undefined;
 
-  // 開始位置を検索文字数ぶんシフト
+  // 開始位置から前を切り落とす
+  const agentSliced = agent.substr(sliceStart);
+
   // バージョン文字数区切りのスペースの位置を探る（開始文字分減じて文字数とする）
-  const end: number = agent.indexOf(endstr, start + sarchStr.length) - start;
+  const end: number = agentSliced.indexOf(endstr);
 
   // バージョン文字を取り出しreturn
-  return agent.substr(start, end).replace('_', '.');
+  return agentSliced.substr(0, end).replace('_', '.');
 };
 
-const browserEnv = (argAgent) => {
-  const agent = argAgent || ua;
+interface envObj {
+  isPC: boolean;
+  isSP: boolean;
+  isIE: boolean;
+  isWebKit: boolean;
+  isiOS: boolean;
+  isAndroid: boolean;
+  browserVer: string,
+  OSVer: string;
+  clickEv: string;
+  scrollEv: string;
+}
+
+const browserEnv = (argAgent) => {// UserAgentを取得
+  const agent: string = argAgent.toLowerCase() || navigator.userAgent.toLowerCase();
   // 返すオブジェクトを仮生成
-  const probBrowser = {
+  const envValue: envObj = {
     isPC: true,
     isSP: false,
     isIE: false,
@@ -29,44 +44,69 @@ const browserEnv = (argAgent) => {
     isAndroid: false,
     browserVer: null,
     OSVer: null,
+    clickEv: null,
+    scrollEv: null,
   };
   // IE
   if (agent.match(/msie|trident/)) {
-    probBrowser.isIE = true;
-    probBrowser.OSVer = `windows ${getVer(agent, 'windows ', ';')}`;
-    probBrowser.browserVer = getVer(agent, 'msie ', ';');
-    if (probBrowser.browserVer === undefined) {
-      probBrowser.browserVer = parseFloat(getVer(agent, 'trident/', ')')) + 4;
+    envValue.isIE = true;
+    // OSバージョン
+    const OSVer = getVer(agent, 'windows ', ';') || getVer(agent, 'windows ', ')');
+    envValue.OSVer = OSVer.match(/[0-9.]+/)[0];
+
+    let MSIEVer = getVer(agent, 'msie ', ';');
+    if (!MSIEVer) {
+      // MSIEでバージョン番号が取得できない場合
+      let tridentVer = parseFloat((getVer(agent, 'trident/', ')')).match(/[0-9.]+/)[0]) + 4;
+      envValue.browserVer = tridentVer.toFixed(1);
+    } else {
+      // MSIEでバージョン番号が取得できた場合
+      envValue.browserVer = MSIEVer;
     }
+
   } else {
     // WebKit
     if (agent.match(/webkit/)) {
-      probBrowser.isWebKit = true;
+      envValue.isWebKit = true;
+      if (agent.indexOf('chrome') !== -1) {
+        envValue.browserVer = getVer(agent, 'chrome/', ' ');
+      } else if (agent.indexOf('safari') !== -1) {
+        envValue.browserVer = getVer(agent, 'version/', ' ');
+      } else {
+        envValue.browserVer = '0';
+      }
     }
     // iOS系
     if (agent.match(/iphone|ipad/)) {
-      probBrowser.isPC = false;
-      probBrowser.isSP = true;
-      probBrowser.isiOS = true;
-      probBrowser.OSVer = getVer(agent, 'iphone os ', ' ');
+      envValue.isPC = false;
+      envValue.isSP = true;
+      envValue.isiOS = true;
+      envValue.OSVer = getVer(agent, 'iphone os ', ' ');
+      envValue.browserVer = envValue.OSVer;
     }
     // Android
     if (agent.match(/android/)) {
-      probBrowser.isPC = false;
-      probBrowser.isSP = true;
-      probBrowser.isAndroid = true;
-
-      probBrowser.OSVer = getVer(agent, 'android ', ';');
+      envValue.isPC = false;
+      envValue.isSP = true;
+      envValue.isiOS = false;
+      envValue.isAndroid = true;
+      envValue.OSVer = getVer(agent, 'android ', ';');
+      envValue.browserVer = envValue.OSVer;
     }
   }
-  window.clickEv = (probBrowser.isPC) ? 'click' : 'touchend';
-  window.scrollEv = (() => {
+
+  // click event
+  envValue.clickEv = (envValue.isPC) ? 'click' : 'touchend';
+  (<any>window).clickEv = envValue.clickEv;
+  // scoll event
+  envValue.scrollEv = (() => {
     if ('onwheel' in document) return 'wheel';
     if ('onmousewheel' in document) return 'mousewheel';
     return 'DOMMouseScroll';
   })();
+  (<any>window).scrollEv = envValue.scrollEv;
   // 仮オブジェクト返す
-  return probBrowser;
+  return envValue;
 };
 
 export default {
--- test/browserEnv.spec.ts
+++ test/browserEnv.spec.ts
@@ -1,19 +1,119 @@
 import {assert} from 'chai';
 import browserEnv from '../src/class/browserEnv';
 
+// test string
+const MSIE7: string = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)";
+const MSIE9: string = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
+const MSIE10: string = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)";
+const MSIE11: string = "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko";
+const CG28: string = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36';
+const SF11: string = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38';
+const iOS6: string = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25';
+const AND4 = 'Mozilla/5.0 (Linux; U; Android 4.1.1; ja-jp; Galaxy Nexus Build/JRO03H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30';
+
 describe('browserEnv', () => {
-  const MSIE9: string = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
-  const MSIE10: string = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)";
   describe('getVer', () => {
+    it('returns undef correctly', () => {
+      assert.isUndefined(browserEnv.getVer(MSIE9, 'mac os ', /;\)/));
+    });
     it('returns OS Version correctly', () => {
-      assert.equal(browserEnv.getVer(MSIE9, 'windows ', ';'), "windows nt 6.1");
-      assert.equal(browserEnv.getVer(MSIE10, 'windows ', ';'), "windows nt 6.2");
+      assert.deepEqual(browserEnv.getVer(MSIE7, 'windows ', ')'), "nt 5.1");
+      assert.deepEqual(browserEnv.getVer(MSIE9, 'windows ', ';'), "nt 6.1");
+      assert.deepEqual(browserEnv.getVer(MSIE10, 'windows ', ';'), "nt 6.2");
     });
-    it('returns OS Version string correctly', () => {
-      assert.equal(browserEnv.getVer(MSIE9, 'trident/', ')'), "trident/5.0");
-      assert.equal(browserEnv.getVer(MSIE10, 'trident/', ')'), "trident/6.0");
+    it('returns Browser Version string correctly in IE(msie)', () => {
+      assert.deepEqual(browserEnv.getVer(MSIE7, 'msie ', ';'), "7.0");
+      assert.deepEqual(browserEnv.getVer(MSIE9, 'msie ', ';'), "9.0");
+      // IE11は'MSIE'を含まないのでUndefinedが返ってくるはず
+      assert.isUndefined(browserEnv.getVer(MSIE11, 'msie ', ';'));
+
     });
-    describe('getVer', () => {
+    it('returns Browser Version string correctly in trident', () => {
+      assert.isUndefined(browserEnv.getVer(MSIE7, 'trident/', ';'));
+      assert.deepEqual(browserEnv.getVer(MSIE9, 'trident/', ')'), "5.0");
+      assert.deepEqual(browserEnv.getVer(MSIE10, 'trident/', ')'), "6.0");
+      assert.deepEqual(browserEnv.getVer(MSIE11, 'trident/', ')'), "7.0; touch; rv:11.0");
+    });
+  });
+  describe('browserEnv', () => {
+    it('defines IE 7', () => {
+      const retVal = browserEnv.browserEnv(MSIE7);
+      assert.isTrue(retVal.isPC);
+      assert.isFalse(retVal.isSP);
+      assert.isTrue(retVal.isIE);
+      assert.isFalse(retVal.isWebKit);
+      assert.deepEqual(retVal.browserVer, '7.0');
+      assert.deepEqual(retVal.OSVer, '5.1');
+    });
+    it('defines IE 9', () => {
+      const retVal = browserEnv.browserEnv(MSIE9);
+      assert.isTrue(retVal.isPC);
+      assert.isFalse(retVal.isSP);
+      assert.isTrue(retVal.isIE);
+      assert.isFalse(retVal.isWebKit);
+      assert.deepEqual(retVal.browserVer, '9.0');
+      assert.deepEqual(retVal.OSVer, '6.1');
+    });
+    it('defines IE 11', () => {
+      const retVal = browserEnv.browserEnv(MSIE11);
+      assert.isTrue(retVal.isPC);
+      assert.isFalse(retVal.isSP);
+      assert.isTrue(retVal.isIE);
+      assert.isFalse(retVal.isWebKit);
+      assert.deepEqual(retVal.browserVer, '11.0');
+      assert.deepEqual(retVal.OSVer, '6.3');
+      assert.deepEqual(retVal.clickEv, 'click');
+    });
+    it('defines Chrome 28', () => {
+      const retVal = browserEnv.browserEnv(CG28);
+      assert.isTrue(retVal.isPC);
+      assert.isFalse(retVal.isSP);
+      assert.isFalse(retVal.isIE);
+      assert.isTrue(retVal.isWebKit);
+      assert.deepEqual(retVal.browserVer, '28.0.1500.52');
+      assert.deepEqual(retVal.OSVer, null);
+      assert.deepEqual(retVal.clickEv, 'click');
+    });
+    it('defines Chrome 28', () => {
+      const retVal = browserEnv.browserEnv(CG28);
+      assert.isTrue(retVal.isPC);
+      assert.isFalse(retVal.isSP);
+      assert.isFalse(retVal.isIE);
+      assert.isTrue(retVal.isWebKit);
+      assert.deepEqual(retVal.browserVer, '28.0.1500.52');
+      assert.deepEqual(retVal.clickEv, 'click');
+    });
+    it('defines Safari 11', () => {
+      const retVal = browserEnv.browserEnv(SF11);
+      assert.isTrue(retVal.isPC);
+      assert.isFalse(retVal.isSP);
+      assert.isFalse(retVal.isIE);
+      assert.isTrue(retVal.isWebKit);
+      assert.deepEqual(retVal.browserVer, '11.0');
+      assert.deepEqual(retVal.OSVer, null);
+      assert.deepEqual(retVal.clickEv, 'click');
+    });
+    it('defines iOS 6', () => {
+      const retVal = browserEnv.browserEnv(iOS6);
+      assert.isFalse(retVal.isPC);
+      assert.isTrue(retVal.isSP);
+      assert.isFalse(retVal.isIE);
+      assert.isTrue(retVal.isWebKit);
+      assert.isTrue(retVal.isiOS);
+      assert.deepEqual(retVal.browserVer, '6.0');
+      assert.deepEqual(retVal.OSVer, '6.0');
+      assert.deepEqual(retVal.clickEv, 'touchend');
+    });
+    it('defines Android 4', () => {
+      const retVal = browserEnv.browserEnv(AND4);
+      assert.isFalse(retVal.isPC);
+      assert.isTrue(retVal.isSP);
+      assert.isFalse(retVal.isIE);
+      assert.isTrue(retVal.isWebKit);
+      assert.isFalse(retVal.isiOS);
+      assert.deepEqual(retVal.browserVer, '4.1.1');
+      assert.deepEqual(retVal.OSVer, '4.1.1');
+      assert.deepEqual(retVal.clickEv, 'touchend');
     });
   });
 });
